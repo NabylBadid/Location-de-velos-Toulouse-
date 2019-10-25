135dc467c1f7f272ea8519b941e13bff876862ac
NabylBadid <badid.nabyl@yahoo.fr>
2019-10-25 11:47:18 +0200

    ajout de lappui sur les fleches + probleme encore avec le bouton pause

--- script.js
+++ script.js
@@ -27,10 +27,63 @@ window.onload = changeImg;
 
 let bPause = document.getElementById("bDiapo");
 
-function pause() {
+function arreter() {
 	clearInterval(interval);
 	bPause.innerHTML = "";
 	bPause.textContent = "Jouer";
 };
+/*
+function jouer() {
+		setInterval("changeImg()", temps);
+		bPause.innerHTML = "";
+		bPause.textContent = "Pause";
+};*/
 
-bPause.addEventListener("click" , pause);
+bPause.addEventListener("click" , arreter);
+//bPause.removeEventListener("click", jouer);
+
+// Clique gauche et droit 
+
+let bGauche = document.getElementById("flecheG");
+let bDroite = document.getElementById("flecheD");
+
+function reculer() {
+
+	document.slide.src = images[i];
+
+	if ((i <= images.length - 1) && (i !== 0)) {
+		i--;
+	} else {
+		i = images.length - 1;
+	}
+}
+
+function avancer() {
+
+	document.slide.src = images[i];
+
+	if ( (i === 0) || (i < images.length - 1) ) {
+		i++;
+	} else {
+		i = 0
+	}
+}
+
+bGauche.addEventListener("click", reculer);
+bDroite.addEventListener("click", avancer);
+
+// Appui sur les flèches de gauche et de droite
+
+function appuyer(e) {
+	if (e.keyCode === 37) {
+		reculer();
+	} else if (e.keyCode === 39) 
+		avancer();
+ }
+
+document.addEventListener("keydown", appuyer);
+
+
+
+
+console.log(images.length);
