4e8d92a98a9220e05b4d9873b77ba3fc457fdcdf
NabylBadid <badid.nabyl@yahoo.fr>
2019-11-09 10:00:18 +0100

    carte affichee correctement

--- script.js
+++ script.js
@@ -223,3 +223,19 @@ function b(message){
 
 a(b);
 */
+
+
+/* Carte */
+
+	let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+	let tuileRue = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: 'pk.eyJ1IjoibmFieWwiLCJhIjoiY2sycHltd2p1MDY5MTNlbW9mODNqdTNhbCJ9.ZBB6hmUZi_JNNjbhURXljw'
+})
+	tuileRue.addTo(mymap);
+
+
+
